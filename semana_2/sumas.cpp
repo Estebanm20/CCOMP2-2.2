dc32fff363b16d4eee2a8fef80474f2beea54bbd
Estebanm20 <esteban.medina@ucsp.edu.pe>
2022-09-01 08:21:16 -0500

    sumas

---
+++ semana_2/sumas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a = 20;
+    int b;
+    b = 16;
+
+    int suma = a+b;
+    int resta = a-b;
+    int multiplicacion = a* b;
+    int division = a/b;
+
+    cout<<"suma de a y b:  "<< suma<<endl;
+    cout<<"resta de a y b: "<< resta<<endl;
+    cout<<"multiplicacion de a y b: "<<multiplicacion<<endl;
+    cout<<"Division de a y b: "<<division<<endl;
+
+    return 0;
+}
